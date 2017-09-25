e1c20c6b02bec73808ec7074fda1ea685a477973
石文文 <shiwenwendevelop@163.com>
2017-09-25 09:25:42 +0800

    分类详情点击进入视频详情

--- Components/Category/CategoryDetailPage.js
+++ Components/Category/CategoryDetailPage.js
@@ -81,6 +81,23 @@ export default class CategoryDetailPage extends Component {
     _onPressItme = (model,index) => {
         console.log('点击了'+index)
         const { navigate } = this.props.navigation;
+        navigate(
+            'VideoDetail',
+            {
+                title:model.title,
+                id:model.id,
+                barcode:model.barcode,
+                play_conver:model.playcover,
+                player:model.player,
+                sys_ctime:model.sys_ctime,
+                cat_text:model.cat_text,
+                count:model.count,
+                cover:model.cover,
+                up_time: model.up_time,
+                category: model.category,
+                cat: model.cat
+            }
+        )
 
     }
     /*渲染分割线*/
